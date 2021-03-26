c050c598f0070efa93e0ffe2d2542f629a4033fd
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-03-26 21:22:12 +0000

    First draft of improved unit test

--- main.clj
+++ main.clj
@@ -1,14 +1,28 @@
-; Remko en Zeger 
-; '(1 2 3)     ; list
-;(println "Hello world")
+; ----------------- 
+; Unit test library
+; -----------------
 
-; (defn greet  [name]  (str "Hello, " name) )
-; (defn test-greet-returns-zeger )
+(defmacro fn-name
+  [f]
+  `(-> ~f var meta :name str))
 
-;(defn assert-equals [string1, string2] 
-;  (if (= string1 string2) true false)
-;)
-;(println (assert-equals (str 4) "4"))
+(defn assert-equals [actual, expected] 
+  (if (= actual expected) 
+    (println "Test ok") 
+    (println (str "test failed, expected " expected " but was " actual)))
+)
+
+(defn execute-test [test-function, expected]
+  (assert-equals (test-function) expected)
+  ;(println 
+    ;(str "Executing " (fn-name test-function) " -> " (test-function))
+  ;  (str "->" (test-function))
+  ;)
+)
+
+(defn my-add-unit-test [] (+ 4 5))
+(execute-test my-add-unit-test 9)
+
 
 ;(println (alwaysfalse "dummy"))
 ;(defn alwaysfalse [ignore] (boolean true))
@@ -22,14 +36,20 @@
 ; Greed code
 ; ------
 
-(defn score [die1 die2]
-  (+ (scoreForSingleDie die1) (scoreForSingleDie die2))
+(defn score [dice]
+  (reduce addScoreForSingleDie 0,dice)
 )
 
-(defn scoreForSingleDie [die]
+(defn addScoreForSingleDie [currentScore,die] 
+;  (println "values: " currentScore die)
+  (+ currentScore (calculateScoreForSingleDie die))
+)
+
+(defn calculateScoreForSingleDie [die]
   (cond
     (= die 1) 100
     (= die 5) 50
+    ; Don't change below code as per request
     :else 0
   )
 )
@@ -37,15 +57,20 @@
 ; Test
 ; ------
 ; testSixDiceContainsSingleOneRewards100  
-(if (= (score 1 2) 100) true (println "Test failed, the value is" (score 1 2)))
+(if (= (score [1 2]) 100) true false)
 
 ; testSixDiceContainsZeroOneRewards0
-(if (= (score 3 4) 0) true false)
+(if (= (score [3 4]) 0) true false)
 
 ; testSixDiceContainsSingleFiveRewards50
-(if (= (score 4 5) 50) true false)
+(if (= (score [4 5]) 50) true false)
 
-(if (= (score 6 4) 0) true false)
+(if (= (score [6 4]) 0) true false)
 
 ; 1 + 5 = 150
-(if (= (score 1 5) 150) true false)
+(if (= (score [1 5]) 150) true false)
+
+;(defn test [x,y]
+;  (+ x 1)
+;)
+;(reduce test [1 2 3 4 5])
