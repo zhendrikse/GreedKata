bd53324181f89f513697918223ebc9b3bac4db12
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-03-27 11:20:03 +0000

    Added triple one

--- main.clj
+++ main.clj
@@ -20,16 +20,16 @@
        (filter #{s})
        count))
 
-(defn score-ones [thrown-dice-list] 
-  (if (= (count-occurrences 1 thrown-dice-list) 1) 100 0)
-)       
-
-(defn score-fives [thrown-dice-list] 
-  (if (= (count-occurrences 5 thrown-dice-list) 1) 50 0)
-)       
+(defn score-occurrences [thrown-dice-list value times score]
+  (if (= (count-occurrences value thrown-dice-list) times) score 0)
+)
 
 (defn score [thrown-dice-list]
-  (+ (score-ones thrown-dice-list) (score-fives thrown-dice-list))
+  (+ 
+    (score-occurrences thrown-dice-list 1 1 100)  ; single one -> 100
+    (score-occurrences thrown-dice-list 5 1 50)   ; single five -> 50
+    (score-occurrences thrown-dice-list 1 3 1000) ; tripe one -> 1000 
+  )
 )
 
 ; ------------------
@@ -46,3 +46,5 @@
 (execute-test "double five in list" 0 (score '(5 4 2 4 5 6)))
 
 (execute-test "single five and single in list" 150 (score '(1 4 2 4 5 6)))
+
+(execute-test "triple one in list" 1000 (score '(1 2 4 6 1 1)))
