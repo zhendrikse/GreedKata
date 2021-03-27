91ce5f3c6f590980d0f63842510a1c0c3dd9e9f0
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-03-27 11:26:19 +0000

    Added some more rules

--- main.clj
+++ main.clj
@@ -29,6 +29,11 @@
     (score-occurrences thrown-dice-list 1 1 100)  ; single one -> 100
     (score-occurrences thrown-dice-list 5 1 50)   ; single five -> 50
     (score-occurrences thrown-dice-list 1 3 1000) ; tripe one -> 1000 
+    (score-occurrences thrown-dice-list 2 3 200)  ; tripe one -> 1000 
+    (score-occurrences thrown-dice-list 3 3 300)  ; tripe one -> 1000 
+    (score-occurrences thrown-dice-list 4 3 400)  ; tripe one -> 1000 
+    (score-occurrences thrown-dice-list 5 3 500)  ; tripe one -> 1000 
+    (score-occurrences thrown-dice-list 6 3 600)  ; tripe one -> 1000 
   )
 )
 
@@ -36,15 +41,14 @@
 ; U n i t  t e s t s
 ; ------------------
 (execute-test "single one in list" 100  (score '(1 4 2 4 6 6)))
-
 (execute-test "not a single one in list" 0 (score '(3 4 2 4 6 6))) 
-
-(execute-test "double one in list" 0 (score '(1 4 1 4 4 6)))
-
+(execute-test "double one in list" 0 (score '(1 4 1 4 6 6)))
 (execute-test "single five in list" 50 (score '(3 4 2 4 5 6)))
-
 (execute-test "double five in list" 0 (score '(5 4 2 4 5 6)))
-
 (execute-test "single five and single in list" 150 (score '(1 4 2 4 5 6)))
-
 (execute-test "triple one in list" 1000 (score '(1 2 4 6 1 1)))
+(execute-test "triple two in list" 200 (score '(2 6 4 6 2 2)))
+(execute-test "triple three in list" 300 (score '(3 2 4 6 3 3)))
+(execute-test "triple four in list" 400 (score '(3 2 4 6 4 4)))
+(execute-test "triple five in list" 500 (score '(5 2 4 6 5 5)))
+(execute-test "triple six in list" 600 (score '(3 2 4 6 6 6)))
